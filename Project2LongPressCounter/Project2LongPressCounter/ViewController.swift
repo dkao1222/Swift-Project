b3d4dc607d946b87bbbd399cba9107af8472835d
Dragon Kao <dkao1222@gmail.com>
2016-05-25 01:08:17 +0800

    Project 2 Long Press Counter
    Swift Project,
    Project 2 :: Long Press Counter

---
+++ Project2LongPressCounter/Project2LongPressCounter/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Project2LongPressCounter
+//
+//  Created by KaoDragon on 2016/5/25.
+//  Copyright © 2016年 KaoDragon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var step : Int = 0
+    
+    @IBOutlet weak var CounterLabel: UILabel!
+    
+    @IBAction func tapButton(sender: AnyObject) {
+        step = step + 1
+        updateLabel()
+    }
+        
+    @IBAction func longPressButton(sender: UILongPressGestureRecognizer) {
+        if sender.state == .Changed {
+            step = step + 1
+            updateLabel()
+        }
+    }
+    @IBAction func resetButton(sender: AnyObject) {
+        step = 0
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        CounterLabel.text = String(step)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        step = 0
+        updateLabel()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
