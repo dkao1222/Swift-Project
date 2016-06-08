56a37ecfd441b433786bbcd711e9b8653e7ae6a6
Dragon Kao <dkao1222@gmail.com>
2016-06-08 13:07:28 +0800

    Project 3 iAdTest

---
+++ Project3iADTest/Project3iADTest/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Project3iADTest
+//
+//  Created by KaoDragon on 2016/6/1.
+//  Copyright © 2016年 KaoDragon. All rights reserved.
+//
+
+import UIKit
+import GoogleMobileAds
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var bannerView: GADBannerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Google Mobile Ads SDK version:" + GADRequest.sdkVersion())
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.loadRequest(GADRequest())
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
